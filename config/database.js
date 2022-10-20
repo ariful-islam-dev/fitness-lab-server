994a9c961b125d9af70e97af92efd5fce7d6f225
ariful4082 <ariful4082@gmail.com>
2022-10-20 07:46:53 +0600

    connected the heroku postgres database

--- config/database.js
+++ config/database.js
@@ -1,11 +1,29 @@
-const path = require('path');
+const parse = require('pg-connection-string').parse;
+    const config = parse(process.env.DATABASE_URL);
+    module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false
+          },
+        },
+        debug: false,
+      },
+    });
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//     },
+//     useNullAsDefault: true,
+//     debug: false,
+//   },
+// });
