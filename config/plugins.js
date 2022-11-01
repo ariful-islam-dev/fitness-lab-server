49b956eb90ece85aab4b68b6841db3fba087e5c9
ariful4082 <ariful4082@gmail.com>
2022-11-01 07:40:34 +0600

    cluodnay issus

--- config/plugins.js
+++ config/plugins.js
@@ -6,9 +6,9 @@
         config: {
           provider: 'cloudinary',
           providerOptions: {
-            cloud_name: process.env.CLOUDINARY_NAME,
-            api_key: process.env.CLOUDINARY_KEY,
-            api_secret: process.env.CLOUDINARY_SECRET
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
           },
           actionOptions: {
             upload: {},
