399a4b189a86b91a1363f3c272de4db1932839c1
ariful4082 <ariful4082@gmail.com>
2022-11-04 18:57:06 +0600

    strapi image configuration

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,7 +14,6 @@ module.exports = [
       },
     },
   },
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
--- config/plugins.js
+++ config/plugins.js
@@ -1,32 +1,31 @@
-
-
-    module.exports = ({ env }) => ({
-      // ...
-      upload: {
-        config: {
-          provider: 'cloudinary',
-          providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME'),
-            api_key: env('CLOUDINARY_KEY'),
-            api_secret: env('CLOUDINARY_SECRET'),
-          },
-          actionOptions: {
-            upload: {},
-            delete: {},
-          },
-        },
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
-      graphql: {
-        config: {
-          endpoint: '/graphql',
-          shadowCRUD: true,
-          playgroundAlways: true,
-          depthLimit: 10,
-          amountLimit: 100,
-          apolloServer: {
-            tracing: false,
-            introspection: true,
-          },
-        },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
-    });
+    },
+  },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 10,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+        introspection: true,
+      },
+    },
+  },
+});
